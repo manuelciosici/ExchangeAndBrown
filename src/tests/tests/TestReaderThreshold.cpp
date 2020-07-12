0beeed9f6ad4d7d06c39bc53f21eff6971414f88
Manuel R. Ciosici <manuelrciosici@gmail.com>
2020-07-12 08:11:09 -0400

    Address issue #1

--- src/tests/tests/TestReaderThreshold.cpp
+++ src/tests/tests/TestReaderThreshold.cpp
@@ -2,47 +2,38 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <readers/ReaderThreshold.h>
+#include <readers/ReaderNoOrder.h>
 
-Corpus createSimpleCorpusThreshold(){
-    //    the corpus here is a a b c d c a a
-    Corpus abcdCorpus;
-    abcdCorpus.vocabularySize = 4;
-    abcdCorpus.corpusLength = 8;
-    abcdCorpus.pl = {3, 1, 2, 1};
-    const word_type no_transitions = abcdCorpus.getNumberOfTransitions();
-    abcdCorpus.pr = {3, 1,
-                     2, 1};
-    abcdCorpus.idsToWords.insert({0, "a"});
-    abcdCorpus.idsToWords.insert({1, "b"});
-    abcdCorpus.idsToWords.insert({2, "c"});
-    abcdCorpus.idsToWords.insert({3, "d"});
-    abcdCorpus.occurrences.insert({{0,0}, 2});
-    abcdCorpus.occurrences.insert({{0,1}, 1});
-    abcdCorpus.occurrences.insert({{1,2}, 1});
-    abcdCorpus.occurrences.insert({{2,0}, 1});
-    abcdCorpus.occurrences.insert({{2,3}, 1});
-    abcdCorpus.occurrences.insert({{3,2}, 1});
-    abcdCorpus.wordCountAsNumbers.insert({0,4});
-    abcdCorpus.wordCountAsNumbers.insert({1,1});
-    abcdCorpus.wordCountAsNumbers.insert({2,2});
-    abcdCorpus.wordCountAsNumbers.insert({3,1});
+TEST(ReaderThresholdTest, testReaderThresholdSimpleCorpusStrict) {
+    ReaderNoOrder reader;
+    const Corpus c = reader.readFile("tests/test_data/abcd.txt");
 
-    return abcdCorpus;
-}
+    EXPECT_EQ(c.vocabularySize, 4);
+    EXPECT_EQ(c.corpusLength, 8);
 
-TEST(ReaderThresholdTest, testReaderThresholdSimpleCorpusNotStrict) {
-    const Corpus abcdCorpus = createSimpleCorpusThreshold();
-    ReaderThreshold reader;
-    const Corpus filteredCorpus = reader.reorderCorpus(abcdCorpus, 2, true);
+    ReaderThreshold readerThreshold;
+    const Corpus filteredCorpus = readerThreshold.reorderCorpus(c, 2, true);
+
     EXPECT_EQ(filteredCorpus.vocabularySize, 2);
     EXPECT_EQ(filteredCorpus.corpusLength, 8);
-    const vector<double> expectedPl = {3, 2};
-    const vector<double> expectedPr = {3, 2};
-    EXPECT_THAT(filteredCorpus.pl, ::testing::ContainerEq(expectedPl));
-    EXPECT_THAT(filteredCorpus.pr, ::testing::ContainerEq(expectedPr));
 
-    occurrence_type expectedOccurrences = {{{0,0}, 2},
-                                           {{1,0}, 1}
+    const vector<double> expectedPl = {0.42857, 0.2857};
+    const vector<double> expectedPr = {0.42857, 0.2857};
+
+    for (int i = 0; i < expectedPl.size(); ++i) {
+        EXPECT_NEAR(filteredCorpus.pl[i], expectedPl[i],
+                    0.001) << "Pl differs at position " << i << ". Expected " << expectedPl[i] << " actual "
+                           << filteredCorpus.pl[i];
+    }
+    for (int i = 0; i < expectedPr.size(); ++i) {
+        EXPECT_NEAR(filteredCorpus.pr[i], expectedPr[i], 0.001)
+                            << "Pr differs at position " << i << ". Expected " << expectedPl[i] << " actual "
+                            << filteredCorpus.pl[i];
+    }
+
+    occurrence_type expectedOccurrences = {{{0, 0}, 2},
+                                           {{1, 0}, 1}
     };
     EXPECT_THAT(filteredCorpus.occurrences, ::testing::ContainerEq(expectedOccurrences));
 }
+
